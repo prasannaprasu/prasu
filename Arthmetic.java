26c21fe67d2271937f39cf641f5c4c2854a082f4
prasannaprasu <prasannajava2714@gmail.com>
2018-04-28 16:44:31 -0400

    Add files via upload
    added newly

---
+++ Arthmetic.java
@@ -0,0 +1,30 @@
+/* 
+Output: 
+Enter two number for Arith Operations:
+5.5
+2.8
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Arithmetic Operations of :5.5and 2.8
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Name : Prasanna 
+Client : Vamsee 
+Date Time : 6/23/2016 and time 12:45PM
+Purpose : Assignment
+*/
+
+class Arthmetic
+{
+	
+	
+	public static void main(String[] args) 
+	{
+		 double n1 =25, n2 =30;
+		 System.out.println("Sum : " + (n1+n2));
+		 System.out.println("Sub : " + (n1-n2));
+		 System.out.println("Div : " + (n1/n2));
+		 System.out.println("Mul : " + (n1*n2));
+		 System.out.println("Mod : " + (n1%n2));
+		 
+		
+	}
+}
